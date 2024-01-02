97075613b4968722c002c8002fd6d7d49fd5c991
Jonny Noone <jonny.f.noone@gmail.com>
2024-01-02 19:08:53 +0000

    01a complete

--- 01/a.js
+++ 01/a.js
@@ -1,1 +1,14 @@
-console.log('Hello');
+const fs = require('node:fs');
+const data = fs.readFileSync('data.txt', 'utf8');
+
+let floor = 0;
+
+for (let i = 0; i < data.length; i++) {
+	if (data[i] === '(') {
+		floor++;
+	} else if (data[i] === ')') {
+		floor--;
+	}
+}
+
+console.log(floor);
