1a9f64eb585703aa30d7b03d166504ad99432456
Jonny Noone <jonny.f.noone@gmail.com>
2024-01-03 17:54:54 +0000

    04a complete

---
+++ 04/a.js
@@ -0,0 +1,16 @@
+const md5 = require('./md5.js');
+
+const secretKey = 'bgvyzdsv';
+let found = false;
+let counter = 1;
+
+while (!found) {
+	let hash = md5(secretKey + counter);
+
+	if (hash.slice(0, 5) === '00000') {
+		console.log(counter);
+		found = true;
+	}
+
+	counter++;
+}
